d0dbd6f27f79c304e33f3e250f006a53de3acca0
alyahumaira2 <alyah4234@gmail.com>
2024-06-23 17:05:17 +0800

    new file:   codemagic.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: MyAppExt());
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: MyAppExt());
   }
 }
 
